0a5e51d48fa9b26fa5001027a3f5c22567b60e57
imyoubin <imyoubin@hanmail.net>
2025-06-05 17:15:02 +0900

    2025.06.05

---
+++ src/com/javaex/ex05/BookApp.java
@@ -0,0 +1,23 @@
+package com.javaex.ex05;
+
+public class BookApp {
+
+	//web_db   web/web
+	//
+	BookDAO bookDAO = new BookDAO();
+	
+	
+	/*
+	--BookVO  book테이블 전용
+	bookDAO.bookInsert();
+	bookDAO.bookUpdate();
+	bookDAO.bookDelete();
+	bookDAO.bookSelect();
+	bookDAO.bookSelectOne();
+	
+	
+	----
+	--BookAuthorVO  book, author테이블 조인
+	bookDAO.bookSelectList() --> 전제조회
+	*/
+}
---
+++ src/com/javaex/ex05/BookVO.java
@@ -0,0 +1,7 @@
+package com.javaex.ex05;
+
+public class BookVO {
+
+	
+	
+}
